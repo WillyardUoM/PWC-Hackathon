fc0ce626ba1a3a2867d3799c4ec20a3ba78664fd
Thahil Purgas <pthahil12@gmail.com>
2023-10-24 14:27:57 +0400

    Logout Completed

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,5 +1,5 @@
-
-import { HashRouter, Route, Routes } from 'react-router-dom';
+/* eslint-disable no-unused-vars */
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primeicons/primeicons.css";
 import Education from "./newcomers/education";
@@ -15,16 +15,17 @@ import Sidebar from "./Dashboard/components/Sidebar/Sidebar";
 import HomePage from "./Dashboard/pages/HomePage";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./Dashboard/styles/theme";
-import './Dashboard/styles/globalStyles'
-import './Dashboard/styleOver.css'
+import "./Dashboard/styles/globalStyles";
+import "./Dashboard/styleOver.css";
 import Register from "./newcomers/register";
 import Proceed from "./newcomers/proceed";
 import UploadResume from "./newcomers/upload_resume";
 import SkillsAssessment from "./newcomers/skills";
 import Others from "./newcomers/other";
 import Login from "./newcomers/login";
-import Profile from './Dashboard/pages/profile';
-import Chatbox from './Dashboard/pages/Chatbot/chatbot';
+import Profile from "./Dashboard/pages/profile";
+import Chatbox from "./Dashboard/pages/Chatbot/chatbot";
+import Logout from "./FirebaseComponent/Logout";
 export const ThemeContext = React.createContext(null);
 
 function App() {
@@ -33,31 +34,30 @@ function App() {
   return (
     <>
       <HashRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route exact path="/Proceed" element={<Proceed />} />
+          <Route path="/Upload_Resume" element={<UploadResume />} />
+          <Route exact path="/Education" element={<Education />} />
+          <Route exact path="/Experience" element={<Experience />} />
+          <Route exact path="/Career_Goals" element={<CareerGoal />} />
+          <Route
+            exact
+            path="/Skill_Assessment"
+            element={<SkillsAssessment />}
+          />
+          <Route exact path="/Others" element={<Others />} />
+          <Route exact path="/LinkedinUrl" element={<LinkedInURL />} />
+        </Routes>
 
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/Register" element={<Register />} />
-              <Route exact path="/Proceed" element={<Proceed />} />
-              <Route path="/Upload_Resume" element={<UploadResume />} />
-              <Route exact path="/Education" element={<Education />} />
-              <Route exact path="/Experience" element={<Experience />} />
-              <Route exact path="/Career_Goals" element={<CareerGoal />} />
-              <Route exact path="/Skill_Assessment" element={<SkillsAssessment />} />
-              <Route exact path="/Others" element={<Others />} />
-              <Route exact path="/LinkedinUrl" element={<LinkedInURL />} />
-            </Routes>
-
-
-
-            <Routes>
-              <Route path="/Dashboard" element={<HomePage />} />
-              <Route path="/Academy" element={<Academy />} />
-              <Route path="/Profile" element={<Profile />} />
-              <Route path="/Chatbox" element={<Chatbox />} />
-
-
-            </Routes>
-
+        <Routes>
+          <Route path="/Dashboard" element={<HomePage />} />
+          <Route path="/Academy" element={<Academy />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Chatbox" element={<Chatbox />} />
+          <Route path="/Logout" element={<Logout />} />
+        </Routes>
       </HashRouter>
     </>
   );
--- React/src/Dashboard/components/Sidebar/Sidebar.jsx
+++ React/src/Dashboard/components/Sidebar/Sidebar.jsx
@@ -1,133 +1,122 @@
-import React, { useContext, useRef, useState } from "react";
+/* eslint-disable no-unused-vars */
+import { useContext, useRef, useState } from "react";
 import {
-    SDivider,
-    SLink,
-    SLinkContainer,
-    SLinkIcon,
-    SLinkLabel,
-
-    SLogo,
-    SSidebar,
-    SSidebarButton,
-
+  SDivider,
+  SLink,
+  SLinkContainer,
+  SLinkIcon,
+  SLinkLabel,
+  SLogo,
+  SSidebar,
+  SSidebarButton,
 } from "./styles";
 
 import {
-    AiOutlineHome,
-    AiOutlineCalendar,
-    AiOutlineFieldTime,
-    AiOutlineLeft,
-    AiOutlineSetting,
-    AiOutlineRobot
+  AiOutlineHome,
+  AiOutlineCalendar,
+  AiOutlineFieldTime,
+  AiOutlineLeft,
+  AiOutlineSetting,
+  AiOutlineRobot,
 } from "react-icons/ai";
 
 import { MdOutlineSchool } from "react-icons/md";
 import { MdLogout, MdOutlineAnalytics } from "react-icons/md";
-import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
-import { Toast } from 'primereact/toast';
-
-
+import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
+import { Toast } from "primereact/toast";
 
 import { useLocation } from "react-router-dom";
 
 const Sidebar = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { pathname } = useLocation();
 
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-    const { pathname } = useLocation();
+  return (
+    <SSidebar isOpen={sidebarOpen}>
+      <>
+        <SSidebarButton
+          isOpen={sidebarOpen}
+          onClick={() => setSidebarOpen((p) => !p)}
+        >
+          <AiOutlineLeft />
+        </SSidebarButton>
+      </>
+      <SLogo>
+        <img src="/images/pwc-logo.png" alt="" />
+      </SLogo>
 
+      <SDivider />
+      {linksArray.map(({ icon, label, to }) => (
+        <SLinkContainer key={label} isActive={pathname === to}>
+          <SLink to={to} style={!sidebarOpen ? { width: `fit-content` } : {}}>
+            <SLinkIcon>{icon}</SLinkIcon>
+            {sidebarOpen && (
+              <>
+                <SLinkLabel>{label}</SLinkLabel>
+              </>
+            )}
+          </SLink>
+        </SLinkContainer>
+      ))}
+      <SDivider />
+      {thirdLinksArray.map(({ icon, label, to }) => (
+        <SLinkContainer key={label} isActive={pathname === to}>
+          <SLink to={to} style={!sidebarOpen ? { width: `fit-content` } : {}}>
+            <SLinkIcon>{icon}</SLinkIcon>
+            {sidebarOpen && <SLinkLabel>{label}</SLinkLabel>}
+          </SLink>
+        </SLinkContainer>
+      ))}
+      <SDivider />
 
-    return (
-        <SSidebar isOpen={sidebarOpen}>
-            <>
-                <SSidebarButton isOpen={sidebarOpen} onClick={() => setSidebarOpen((p) => !p)}>
-                    <AiOutlineLeft />
-                </SSidebarButton>
-            </>
-            <SLogo>
-                <img src="/images/pwc-logo.png" alt="" />
-
-            </SLogo>
-
-            <SDivider />
-            {linksArray.map(({ icon, label, to }) => (
-                <SLinkContainer key={label} isActive={pathname === to}>
-                    <SLink to={to} style={!sidebarOpen ? { width: `fit-content` } : {}}>
-                        <SLinkIcon>{icon}</SLinkIcon>
-                        {sidebarOpen && (
-                            <>
-                                <SLinkLabel>{label}</SLinkLabel>
-
-                            </>
-                        )}
-                    </SLink>
-                </SLinkContainer>
-            ))}
-            <SDivider />
-            {thirdLinksArray.map(({ icon, label, to }) => (
-                <SLinkContainer key={label} isActive={pathname === to}>
-                    <SLink to={to} style={!sidebarOpen ? { width: `fit-content` } : {}}>
-                        <SLinkIcon>{icon}</SLinkIcon>
-                        {sidebarOpen && <SLinkLabel>{label}</SLinkLabel>}
-                    </SLink>
-                </SLinkContainer>
-            ))}
-            <SDivider />
-
-            {secondaryLinksArray.map(({ icon, label, to }) => (
-                <SLinkContainer key={label} isActive={pathname === to}>
-                    <SLink to={to} style={!sidebarOpen ? { width: `fit-content` } : {}}>
-                        <SLinkIcon>{icon}</SLinkIcon>
-                        {sidebarOpen && <SLinkLabel>{label}</SLinkLabel>}
-                    </SLink>
-                </SLinkContainer>
-            ))}
-            <SDivider />
-
-        </SSidebar>
-    );
+      {secondaryLinksArray.map(({ icon, label, to }) => (
+        <SLinkContainer key={label} isActive={pathname === to}>
+          <SLink to={to} style={!sidebarOpen ? { width: `fit-content` } : {}}>
+            <SLinkIcon>{icon}</SLinkIcon>
+            {sidebarOpen && <SLinkLabel>{label}</SLinkLabel>}
+          </SLink>
+        </SLinkContainer>
+      ))}
+      <SDivider />
+    </SSidebar>
+  );
 };
 
 const linksArray = [
-    {
-        label: "Home",
-        icon: <AiOutlineHome />,
-        to: "/",
-
-    },
-    {
-        label: "Training Plan",
-        icon: <AiOutlineCalendar />,
-        to: "/calendar",
-
-    },
-
-
+  {
+    label: "Home",
+    icon: <AiOutlineHome />,
+    to: "/",
+  },
+  {
+    label: "Training Plan",
+    icon: <AiOutlineCalendar />,
+    to: "/calendar",
+  },
 ];
 const thirdLinksArray = [
-    {
-        label: "Academy",
-        icon: <MdOutlineSchool />,
-        to: "/Academy",
-
-    },
-    {
-        label: "AI Tutor",
-        icon: <AiOutlineRobot />,
-        to: "/chatbox",
-
-    },
+  {
+    label: "Academy",
+    icon: <MdOutlineSchool />,
+    to: "/Academy",
+  },
+  {
+    label: "AI Tutor",
+    icon: <AiOutlineRobot />,
+    to: "/chatbox",
+  },
 ];
 const secondaryLinksArray = [
-    {
-        label: "Settings",
-        icon: <AiOutlineSetting />,
-        to: "/Profile",
-
-    },
-    {
-        label: "Logout",
-        icon: <MdLogout />,
-    },
+  {
+    label: "Settings",
+    icon: <AiOutlineSetting />,
+    to: "/Profile",
+  },
+  {
+    label: "Logout",
+    icon: <MdLogout />,
+    to: "/Logout",
+  },
 ];
 
 export default Sidebar;
