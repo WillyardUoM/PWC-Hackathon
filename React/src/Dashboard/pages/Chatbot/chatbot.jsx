9038f6ce9299b42f53fb755eadd0d0d89fe39271
Avi1011x <bhavish.seetul1@umail.uom.ac.mu>
2023-10-26 17:25:39 +0400

    wa

--- React/src/Dashboard/pages/Chatbot/chatbot.jsx
+++ React/src/Dashboard/pages/Chatbot/chatbot.jsx
@@ -9,7 +9,7 @@ import { Editor } from "primereact/editor";
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = "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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
